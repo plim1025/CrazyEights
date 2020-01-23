97ca0f8b8ca6572728db6f76ec608c04a33d32ee
plim1025 <plim1025@gmail.com>
2020-01-23 06:42:06 +0000

    Added player class without function testing

---
+++ player.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <cstdlib>
+#include "card.h"
+#include "hand.h"
+#include "player.h"
+
+Player::Player() {
+    hand = Hand();
+    name = "";
+}
+
+Player::Player(Hand init_hand, string init_name) {
+    hand = init_hand;
+    name = init_name;
+}
+
+Player::~Player() {};
+
+Hand Player::get_hand() {
+    return hand;
+}
+
+string Player::get_name() {
+    return name;
+}
+
+void Player::set_hand(Hand new_hand) {
+    hand = new_hand;
+}
+
+void Player::set_name(string new_name) {
+    name = new_name;
+}
+
+void Player::add_card(Card new_card) {
+    hand.add_card(new_card);
+}
+
+void Player::remove_card(int rank, int suit) {
+    hand.remove_card(rank, suit);
+}
+
+bool Player::check_hand(int rank, int suit) {
+    Card *cards = hand.get_cards();
+    for(int i = 0; i < hand.get_n_cards(); i++) {
+        if(cards[i].get_rank() == rank && cards[i].get_suit() == suit)
+            return true;
+    }
+    return false;
+}
+
+int Player::ask_suit() {
+    string suit = "";
+    while(suit != "1" || suit != "2" || suit != "3" || suit != "4") {
+        cout << "Which suit do you want the computer to place? Club - 1, Diamond - 2, Hearts - 3, Spades - 4: ";
+        getline(cin, suit);
+    }
+    return atoi(suit.c_str());
+}
